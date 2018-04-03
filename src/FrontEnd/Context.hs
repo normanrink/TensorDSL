53ed999f7474e7947697526d430e4a5e2ffa13a1
Norman Rink <norman.rink@tu-dresden.de>
2018-04-03 11:16:34 +0200

    Extended the (static) context with state so that fresh variables can be obtained from it.

--- src/FrontEnd/Context.hs
+++ src/FrontEnd/Context.hs
@@ -1,33 +1,46 @@
 
- -- deliberately export every function defined in this file:
-module Context where
+module Context ( Context
+               , emptyCtx
+               , inScope
+               , getSymbol
+               , insertDecl
+               , insertDecls
+               , fromDecls
+               , freshVariable
+               , insertSymbol ) where
 
 
+import Control.Monad.State
 import qualified Data.Map as Map
 
 import qualified Symbol
 import qualified AST
 
 
-type Context = Map.Map String Symbol.Symbol
-
+data Context = Ctx { table :: Map.Map String Symbol.Symbol
+                   , vars  :: Int }
+             deriving (Eq, Show)
 
 emptyCtx :: Context
-emptyCtx = Map.empty
+emptyCtx = Ctx Map.empty 0
 
 inScope :: String -> Context -> Bool
-inScope = Map.member
+inScope name ctx = Map.member name (table ctx)
 
 lookup :: String -> Context -> Maybe Symbol.Symbol
-lookup = Map.lookup
+lookup name ctx = Map.lookup name (table ctx)
+
+insert :: String -> Symbol.Symbol -> Context -> Context
+insert name sym (Ctx table vars) = Ctx (Map.insert name sym table) vars 
+
+incVars :: Context -> Context
+incVars (Ctx table vars) = Ctx table (vars+1)
+
 
 getSymbol :: String -> Context -> Symbol.Symbol
 getSymbol name ctx = case Context.lookup name ctx of
   Nothing  -> error "name not bound in context"
   Just sym -> sym
-
-insert :: String -> Symbol.Symbol -> Context -> Context
-insert = Map.insert
 
 insertDecl :: Context -> AST.Decl -> Context
 insertDecl ctx decl = let name = AST.declName decl
@@ -41,3 +54,12 @@ insertDecls ctx decl = foldl insertDecl ctx decl
 
 fromDecls :: [AST.Decl] -> Context
 fromDecls = insertDecls emptyCtx
+
+freshVariable :: State Context String
+freshVariable = do ctx <- get
+                   put $ incVars ctx
+                   return $ "_t" ++ show (vars ctx)
+
+insertSymbol :: String -> Symbol.Symbol -> State Context ()
+insertSymbol name sym = do ctx <- get
+                           put $ insert name sym ctx
